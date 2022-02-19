e5a395f776a2b9398962d8d2214e11a64250bbcf
vignesh sweekaran <vigneshsweekaran@gmail.com>
2022-02-19 21:34:05 +0530

    Printing patient names

--- assignments/1-json/find-alive-count.py
+++ assignments/1-json/find-alive-count.py
@@ -6,15 +6,19 @@ with open("data.json") as jsonFile:
     data = json.load(jsonFile)
     jsonFile.close()
 
-for key, patient_data in data.items():
+for patient_name, patient_data in data.items():
     if patient_data["status"] == "alive" and patient_data["age"] > 50:
         if patient_data["disease"] in output_data:
-            present_count = int(output_data[patient_data["disease"]])
-            output_data[patient_data["disease"]] = present_count+1
+            present_count = int(output_data[patient_data["disease"]]["count"])
+            output_data[patient_data["disease"]]["count"] = present_count+1
+            output_data[patient_data["disease"]]["patient_names"].append(patient_name)
         else:
-          output_data[patient_data["disease"]] = 1
+          output_data[patient_data["disease"]] = {"count": "1", "patient_names": [patient_name]}
 
-for disease, count in output_data.items():
-    print(f"{disease} : {count}")
+for disease, details in output_data.items():
+    count = details["count"]
+    patient_names = ", ".join(details["patient_names"])
+    print(f" No of patient's infected with {disease} is {count} and the patient names are {patient_names}")
+
         
 
