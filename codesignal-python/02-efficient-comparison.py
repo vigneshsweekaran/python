f063a50b6044e8656166c37bdad4e22310bd858b
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2022-08-25 10:41:49 +0000

    updated

---
+++ codesignal-python/02-efficient-comparison.py
@@ -0,0 +1,12 @@
+# program to find, whether he suqare output lies within the range
+
+def check(x,y,l,r):
+    if l < x ** y <= r:
+        print(True)
+    if x ** y > l and x ** y <= r:
+        print(True)
+    if x ** y in range(l+1, r+1):
+        print(True)
+    
+
+check(2,3,4,10)
