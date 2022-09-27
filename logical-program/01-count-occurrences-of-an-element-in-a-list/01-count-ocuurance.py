6b8fd7e5935e758aef99d4ab2db2a72f4c754bc7
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2022-09-27 13:11:33 +0000

    updated

---
+++ logical-program/01-count-occurrences-of-an-element-in-a-list/01-count-ocuurance.py
@@ -0,0 +1,15 @@
+# We keep a counter that keeps on increasing if the required element is found in the list.
+
+
+def count_occurence(list, x):
+    counter = 0
+    for element in list:
+        if element == x:
+            counter = counter+1
+    return counter
+
+
+data = [8, 6, 8, 10, 8, 20, 10, 8, 8]
+FIND_OCCURANCE=8
+OUTPUT = count_occurence(data, FIND_OCCURANCE)
+print(f"{FIND_OCCURANCE} has occurred {OUTPUT} times")
---
+++ logical-program/01-count-occurrences-of-an-element-in-a-list/README.md
@@ -0,0 +1,4 @@
+## Count occurrences of an element in a list
+
+### Reference
+* https://www.geeksforgeeks.org/python-count-occurrences-element-list/
