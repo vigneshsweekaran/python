5b09d220f8f0e7bcabcad9f79ce8e750904e45d6
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2022-12-21 15:29:21 +0000

    updated

--- 01-logical-program/05-weather-app/main.py
+++ 01-logical-program/05-weather-app/main.py
@@ -6,11 +6,12 @@ def print_weather_data(city):
     url = "https://api.weatherapi.com/v1/current.json?key="+API_KEY+"&q="+city+"&aqi=no"
     res = requests.get(url)
     data = res.json()
+    choosed_city = data["location"]["name"]
     temperature_in_celsius = data["current"]["temp_c"]
     temperature_in_fahrenheit = data["current"]["temp_f"]
     wind_kph = data["current"]["wind_kph"]
     humidity = data["current"]["humidity"]
-    print(f"City                      : {city}")
+    print(f"\nCity                      : {choosed_city}")
     print(f"Temperature in celsius    : {temperature_in_celsius}")
     print(f"Temperature in fahrenheit : {temperature_in_fahrenheit}")
     print(f"Wind speed in kph         : {wind_kph}")
@@ -18,20 +19,25 @@ def print_weather_data(city):
 
 
 def main():
-    city = getUserInput('Enter the city: ')
-    if city == 1:
-
+    print("Weather app to give the temperature, wind speed and humidity details \n")
+    print("1 - Kanyakumari")
+    print("2 - Tirunelveli")
+    print("3 - Madurai")
+    print("4 - Chennai")
+    print("5 - Delhi\n")
+    choice = int(input('Choose the city from above list : '))
+    if choice == 1:
+        print_weather_data("kanyakumari")
     elif choice == 2:
-        ...
-        n -= 1
+        print_weather_data("tirunelveli")
     elif choice == 3:
-        ...
-        n -= 1
+        print_weather_data("madurai")
+    elif choice == 4:
+        print_weather_data("chennai")
+    elif choice == 5:
+        print_weather_data("delhi")
     else:
-        print 'Please choose the correct city name'
-    
-    print_weather_data(city)
-
+        print("\nPlease choose the correct city from the above list")
 
 
 if __name__=='__main__':
